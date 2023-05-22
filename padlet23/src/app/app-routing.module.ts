9e0bf0915df0e85631fe0bc818b58ee78c5c094d
patritt <s2010456025@fhooe.at>
2023-05-22 23:39:28 +0200

    Adapt api.php and routing

--- padlet23/src/app/app-routing.module.ts
+++ padlet23/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {PadletDetailsComponent} from "./padlet-details/padlet-details.component"
 import {EntrieActionsComponent} from "./entrie-actions/entrie-actions.component";
 import {PadletActionsComponent} from "./padlet-actions/padlet-actions.component";
 import {LoginComponent} from "./login/login.component";
+import {CommentsRatingsComponent} from "./comments-ratings/comments-ratings.component";
 
 const routes: Routes = [
   {path:'', redirectTo: 'home', pathMatch:'full'},
@@ -15,6 +16,7 @@ const routes: Routes = [
   {path:'padlets/:id/entry/:entrie_id', component: EntrieActionsComponent},
   {path:'padlets/padlet/:id', component: PadletActionsComponent},
   {path:'login', component: LoginComponent},
+  {path:'comments/:id/:entrie_id', component: CommentsRatingsComponent},
 ];
 
 @NgModule({
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\EntrieController;
 use App\Http\Controllers\PadletController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserRightsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,10 +28,22 @@ Route::get('padlets', [PadletController::class,'index']);
 Route::get('padlets/padlet/{id}', [PadletController::class,'getPadlet']);
 Route::get('padlets/{id}',[EntrieController::class, 'getAllEntries']);
 Route::get('padlets/{id}/{entrie_id}',[EntrieController::class, 'getEntrie']);
+Route::get('users',[UserController::class, 'index']);
+
+//todo baustelle =============
+//Route::get('commentusers/{padlet_id}',[PadletController::class, 'getUserNamesOfComments']);
+
+//todo baustelle =============
+//Route::get('userrights/{user_id}',[UserRightsController::class, 'getCurrentUserRights']);
+
 Route::post('padlets', [PadletController::class,'savePadlets']);
 Route::post('padlets/{id}', [EntrieController::class,'saveEntries']);
+
 Route::put('padlets/{id}', [PadletController::class,'updatePadlets']);
 Route::put('padlets/{id}/{entrie_id}', [EntrieController::class,'updateEntries']);
+Route::put('comments/{id}/{entrie_id}', [EntrieController::class, 'likeEntrie']);
+//Route::put('comments/{id}/{entrie_id}', [EntrieController::class, 'commentEntrie']);
+
 Route::delete('padlets/{id}', [PadletController::class,'deletePadlet']);
 Route::delete('padlets/{id}/{entrie_id}', [EntrieController::class,'deleteEntrie']);
 
